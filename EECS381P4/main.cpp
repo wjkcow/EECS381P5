f94ef047a369e67acd65495d26b3d337a4b1ea95
Jingkui Wang <wjkcow@gmail.com>
2015-03-05 17:10:07 -0500

    Initial Commit

---
+++ EECS381P4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  EECS381P4
+//
+//  Created by wjkcow on 3/5/15.
+//  Copyright (c) 2015 wjkcow. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
